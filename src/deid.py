7fed8644226f09c6906117d9f5850c5b8d74abca
Seunghyeon Kim <seunghyeon.me@gmail.com>
2025-05-20 16:56:04 +0900

    .

---
+++ src/deid.py
@@ -0,0 +1,12 @@
+from src.utils.metric import VideoMetrics
+
+
+class DeID:
+    def __init__(self):
+        pass
+
+    def update_deidzone(self):
+        pass
+
+    def deidentify_face(self):
+        pass
